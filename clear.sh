93504f3d3123f50ec191b800885d26d42edbe3c9
Vianney RIOTTE <v.riotte@myunisoft.fr>
2025-11-27 12:25:25 +0100

    db

--- clear.sh
+++ clear.sh
@@ -1,3 +1,3 @@
-rm -f prisma/dev.db && npx prisma db push
-rm -f ./dev.db && npx prisma db push
-rm -rf .next && npx prisma generate
+# rm -f prisma/dev.db && npx prisma db push
+# rm -f ./dev.db && npx prisma db push
+rm -rf .next && rm -f ./dev.db && npx prisma generate && npx prisma db push
